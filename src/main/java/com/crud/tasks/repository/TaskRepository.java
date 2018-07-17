cc8bb7bfd8be71d8ca1a8035ae0764e0d4aa7aa0
kman66 <karol.manterys@gmail.com>
2018-07-17 15:27:45 +0200

    executing submodule 15.3

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -4,10 +4,14 @@ import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
 
-    Task findTaskById(Long taskId);
+    Optional<Task> findById(Long taskId);
+
+    @Override
+    Task save(Task task);
 }
