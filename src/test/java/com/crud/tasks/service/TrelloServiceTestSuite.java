71c94a12f3d590b004ad67dcd63645748f826b8e
kman66 <karol.manterys@gmail.com>
2018-09-17 16:32:01 +0200

    adding exercise 22.2

---
+++ src/test/java/com/crud/tasks/service/TrelloServiceTestSuite.java
@@ -0,0 +1,51 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.BadgeOfCard;
+import com.crud.tasks.domain.CreatedTrelloCardDto;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrelloServiceTestSuite {
+    @InjectMocks
+    private TrelloService trelloService;
+
+    @Mock
+    private TrelloClient trelloClient;
+
+    @Mock
+    private SimpleEmailService emailService;
+
+    @Mock
+    private AdminConfig adminConfig;
+
+    @Test
+    public void shouldCreateTrelloCard() {
+        //Given
+        CreatedTrelloCardDto createdTrelloCardDto = new CreatedTrelloCardDto("1", "test_name", "test_url", new BadgeOfCard());
+        TrelloCardDto trelloCardDto = new TrelloCardDto("test_name", "test_desc", "1", "2");
+        Mail mail = new Mail("to", "test_subject", "test_message", "toCc");
+        Mockito.when(trelloClient.createNewCard(trelloCardDto)).thenReturn(createdTrelloCardDto);
+        Mockito.doNothing().when(emailService).send(mail);
+        //When
+        CreatedTrelloCardDto returnedCreatedTrelloCardDto = trelloService.createTrelloCard(trelloCardDto);
+        //Then
+        assertNotNull(returnedCreatedTrelloCardDto);
+        assertTrue(returnedCreatedTrelloCardDto instanceof CreatedTrelloCardDto);
+        assertEquals("1", returnedCreatedTrelloCardDto.getId());
+        assertEquals("test_name", returnedCreatedTrelloCardDto.getName());
+        assertEquals("test_url", returnedCreatedTrelloCardDto.getShortUrl());
+        assertNotNull(returnedCreatedTrelloCardDto.getBadges());
+    }
+}
--- src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTest.java
+++ src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTest.java
@@ -1,9 +1,6 @@
 package com.crud.tasks.trello.facade;
 
-import com.crud.tasks.domain.TrelloBoard;
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.domain.TrelloList;
-import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.domain.*;
 import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.validator.TrelloValidator;
@@ -100,4 +97,38 @@ public class TrelloFacadeTest {
         });
     }
 
+    @Test
+    public void shouldCreateCard() {
+        //Given
+        TrelloCardDto trelloCardDto = new TrelloCardDto("my_card", "test_desc", "1", "1");
+        TrelloCard trelloCard = new TrelloCard("my_card", "test_desc", "1", "1");
+        TrelloOfAttachment trelloOfAttachment = new TrelloOfAttachment(1, 2);
+        AttachmentByTypeOfBadge attachmentByTypeOfBadge = new AttachmentByTypeOfBadge(trelloOfAttachment);
+        BadgeOfCard badgeOfCard = new BadgeOfCard(1, attachmentByTypeOfBadge);
+        CreatedTrelloCardDto createdTrelloCardDto = new CreatedTrelloCardDto("1", "my_card", "test_url", badgeOfCard);
+
+        Mockito.when(trelloMapper.mapToCard(trelloCardDto)).thenReturn(trelloCard);
+        Mockito.doNothing().when(trelloValidator).validateCard(trelloCard);
+        Mockito.when(trelloMapper.mapToCardDto(trelloCard)).thenReturn(trelloCardDto);
+        Mockito.when(trelloService.createTrelloCard(trelloCardDto)).thenReturn(createdTrelloCardDto);
+
+        //When
+        CreatedTrelloCardDto testCreatedTrelloCard = trelloFacade.createCard(trelloCardDto);
+
+        //Then
+        assertNotNull(testCreatedTrelloCard);
+        assertEquals("1", testCreatedTrelloCard.getId());
+        assertEquals("my_card", testCreatedTrelloCard.getName());
+        assertEquals("test_url", testCreatedTrelloCard.getShortUrl());
+        assertNotNull(testCreatedTrelloCard.getBadges());
+        assertTrue(testCreatedTrelloCard.getBadges() instanceof BadgeOfCard);
+        assertEquals(1, testCreatedTrelloCard.getBadges().getVotes());
+        assertNotNull(testCreatedTrelloCard.getBadges().getAttachments());
+        assertTrue(testCreatedTrelloCard.getBadges().getAttachments() instanceof AttachmentByTypeOfBadge);
+        assertEquals(attachmentByTypeOfBadge, testCreatedTrelloCard.getBadges().getAttachments());
+        assertTrue(testCreatedTrelloCard.getBadges().getAttachments().getTrello() instanceof TrelloOfAttachment);
+        assertNotNull(testCreatedTrelloCard.getBadges().getAttachments().getTrello());
+        assertEquals(1, testCreatedTrelloCard.getBadges().getAttachments().getTrello().getBoard());
+        assertEquals(2, testCreatedTrelloCard.getBadges().getAttachments().getTrello().getCard());
+    }
 }
